4d79c83f058234e9dcad150cf4da89e9f1d36a82
maikolnzioka-oss <maikolnzioka@gmail.com>
2025-10-24 05:24:45 +0300

    Add files via upload

---
+++ calculateElectricBill.c
@@ -0,0 +1,37 @@
+/*Name:MICHAEL NZIOKA
+  Reg:CT101/G/26528/25
+  Description:Calculatiom of electric bill
+  */
+
+#include <stdio.h>
+
+//Funtion declaration
+float ElectricBill(int units) {
+    float bill;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } 
+    else if (units <= 200) {
+        bill = (100 * 10) + (units - 100) * 15;
+    } 
+    else {
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+    }
+
+    return bill;
+}
+
+int main() {
+	//Funtion call
+    int units;
+    float totalBill;
+   //Prompt the user to enter number of units consumed
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    totalBill = ElectricBill(units);
+    printf("Total electricity bill = KSh. %.2f\n", totalBill);
+
+    return 0;
+}
