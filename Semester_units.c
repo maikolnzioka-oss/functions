3baa118f0d889b14992064ba7f9b3f211ac5bcda
maikolnzioka-oss <maikolnzioka@gmail.com>
2025-10-30 21:22:37 +0300

    Add files via upload

---
+++ Semester_units.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    printf("=====================================\n");
+    printf("   ACADEMIC UNITS OFFERED THIS SEMESTER\n");
+    printf("=====================================\n");
+
+    printf("1. Analogue Electronics\n");
+    printf("2. Discrete Mathematics\n");
+    printf("3. Computer Architecture\n");
+    printf("4. Introduction to Programming and Problem solving\n");
+    printf("5. Communication Skills\n");
+    printf("6. Mathematics for Science\n");
+    printf("7. Physics\n");
+    printf("8. Fundamentals od ICT\n");
+    
+    printf("=====================================\n");
+    printf("       End of List\n");
+    printf("=====================================\n");
+
+    return 0;
+}
